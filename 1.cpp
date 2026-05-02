2d24ee753d5f235caf4ee24776ae5a76cc5062ac
NamanSuthar <ns9257837@gmail.com>
2026-05-02 10:39:13 +0530

    and

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   float x;
+   for(int i = 1; i<=n; i++)
+   {
+       if (i%2==0)
+       cout<<i<<endl;
+   }
+}
+
+    
