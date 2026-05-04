9caaaacc098f92863b520b370374ba25810d06fd
NamanSuthar <ns9257837@gmail.com>
2026-05-04 10:42:32 +0530

    Normal Commit

--- .vscode/Mirrortriangle.cpp
+++ .vscode/Mirrortriangle.cpp
@@ -3,11 +3,14 @@ using namespace std;
 int main()
 {
     int r;
-    cout<<"enter the no. of rows : ";
     cin>>r;
     for(int i=1; i<=r; i++)
     {
         for(int j=1; j<=r-i; j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1; k<=i; k++)
         {
             cout<<"*";
         }
