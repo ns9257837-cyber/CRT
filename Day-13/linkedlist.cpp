5cf2da6d895e1662e587cff21a414b7f9cc47f6e
NamanSuthar <ns9257837@gmail.com>
2026-05-15 08:54:41 +0530

    commit

---
+++ Day-13/linkedlist.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+void printNodes(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Node* head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    printNodes(head);
+    return 0;
+}
